fff3905376c9ed96441fe7b5a67896cc695ad0e3
Iamneo <techadmin@iamneo.ai>
2023-09-21 06:48:37 +0000

    Backup Commit

--- springapp/src/main/resources/java.com.examly.springapp.models/Customer.java
+++ springapp/src/main/resources/java.com.examly.springapp.models/Customer.java
@@ -5,4 +5,36 @@ class Customer {
     private int age;
     private long mobile;
     bill Bill;
+    public int getCustomerId() {
+        return customerId;
+    }
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+    public String getCustomerNmame() {
+        return customerNmame;
+    }
+    public void setCustomerNmame(String customerNmame) {
+        this.customerNmame = customerNmame;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public long getMobile() {
+        return mobile;
+    }
+    public void setMobile(long mobile) {
+        this.mobile = mobile;
+    }
+    public bill getBill() {
+        return Bill;
+    }
+    public void setBill(bill bill) {
+        Bill = bill;
+    }
+
+    
 }
